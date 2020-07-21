2cc290ff8361cc0007197d7202048a3a4ae72779
Chalukya H <62981809+Chalukya-H@users.noreply.github.com>
2020-07-21 12:54:21 +0530

    Updates

---
+++ precache-manifest.311184d608e05f8848ef725a38041244.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "794c27aec9886533bb1249269631c7e5",
+    "url": "/Shopex-New/index.html"
+  },
+  {
+    "revision": "9bf7e4f97df82d93f669",
+    "url": "/Shopex-New/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "bdfb53c3f201a7965f50",
+    "url": "/Shopex-New/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9bf7e4f97df82d93f669",
+    "url": "/Shopex-New/static/js/2.7718402f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Shopex-New/static/js/2.7718402f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdfb53c3f201a7965f50",
+    "url": "/Shopex-New/static/js/main.dcf58300.chunk.js"
+  },
+  {
+    "revision": "9286f7f512b5ebdaec7d",
+    "url": "/Shopex-New/static/js/runtime-main.59e4630b.js"
+  },
+  {
+    "revision": "aaa53939b4dec69c1c602facc5f29205",
+    "url": "/Shopex-New/static/media/Logo-1.aaa53939.PNG"
+  },
+  {
+    "revision": "2ef8e4844c0672d94c78dae2a415f8cc",
+    "url": "/Shopex-New/static/media/image1.2ef8e484.jpg"
+  },
+  {
+    "revision": "e09ce4e7d99552bd43d39055273ef51e",
+    "url": "/Shopex-New/static/media/image2.e09ce4e7.jpg"
+  },
+  {
+    "revision": "d1d39096eea6da491205ca640a2f99ae",
+    "url": "/Shopex-New/static/media/image3.d1d39096.jpg"
+  },
+  {
+    "revision": "2be7db46d64992c8ebdefa98229fe443",
+    "url": "/Shopex-New/static/media/image4.2be7db46.jpg"
+  },
+  {
+    "revision": "54ce5ec09f1d7145978cb9af8be7cf15",
+    "url": "/Shopex-New/static/media/image5.54ce5ec0.jpg"
+  }
+]);
